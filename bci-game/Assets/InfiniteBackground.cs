ec5e596f2e92912f0b55b8a26d1a112262e14935
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2023-10-07 15:22:03 -0400

    chore: internal documentation

--- bci-game/Assets/InfiniteBackground.cs
+++ bci-game/Assets/InfiniteBackground.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class InfiniteBackground : MonoBehaviour
 {
     Camera mainCamera;
-    float tolerance = 5;
-
 
     private void Awake()
     {
@@ -22,11 +20,13 @@ public class InfiniteBackground : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if camera is moving too far to the right
         if (Mathf.Abs(mainCamera.transform.position.x - gameObject.transform.GetChild(1).gameObject.transform.position.x) < 
              Mathf.Abs(mainCamera.transform.position.x - transform.position.x) ) {
 
             transform.position = gameObject.transform.GetChild(1).gameObject.transform.position;
 
+        //if camera is moving too far to the left
         } else if (Mathf.Abs(mainCamera.transform.position.x - gameObject.transform.GetChild(0).gameObject.transform.position.x) <
              Mathf.Abs(mainCamera.transform.position.x - transform.position.x) ) {
 
