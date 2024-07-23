678b8c5786abc212d804f56e83e60eefeee0022a
Yaxin <113393972+yaxinw04@users.noreply.github.com>
2024-07-23 19:50:48 -0400

    fixed some errors yay

--- bci-game/Assets/BoardStreamData.cs
+++ bci-game/Assets/BoardStreamData.cs
@@ -10,8 +10,8 @@ using brainflow.BoardShim;
 using brainflow.DataFilter;
 using brainflow.Enums;
 
-public class BoardStreamData : MonoBehaviour {
-
+public class BoardStreamData : MonoBehaviour
+{
 	private BoardShim board_shim = null;
 	private double sampling_rate = 0;
 	private double[,] data;
@@ -24,74 +24,82 @@ public class BoardStreamData : MonoBehaviour {
 	private double blink_thresh = 0;
 
 	// Use this for initialization
-    void Start () 
+	void Start()
 	{
 		try
 		{
-			BrainFlowInputParams input_params = new BrainFlowInputParams();
+			BrainFlowInputParams inputParams = new BrainFlowInputParams();
 			BoardIds board_id = BoardIds.CROWN_BOARD;
-			inputParams.serial_number = "9bb2e2fad5668286a4b4f407002b4359";
-			sample_rate = BoardShim.get_sampling_rate((int)board_id);
+			inputParams.serial_number = "76dcd666ce9ddeaed072838d28b9c5ac";
+			sampling_rate = BoardShim.get_sampling_rate((int)board_id);
 			BoardShim.enable_dev_board_logger();
 
-			BoardShim board = new BoardShim((int)board_id, inputParams);
-			board.prepare_session();
+			board_shim = new BoardShim((int)board_id, inputParams);
+			board_shim.prepare_session();
 
-			board.start_stream(450000);
+			board_shim.start_stream(450000);
 			Debug.Log("Starting calibration");
 			Thread.Sleep(5000);
-			data = board.get_current_board_data();
+			data = board_shim.get_current_board_data();
 			Debug.Log("start blinking");
 
 			// modularize this loop code into a method
-			while (samples < num_samples) {
-				data = board.get_current_board_data();
-                if (data[1].Length > 0) {
-					DataFilter.perform_rolling_filter(data[1], 2, AggOperations.MEAN);
-                    vals_mean += DataFilter.get_avg(data[1]) / num_samples;
-                    samples += data[1].Length;
-                    if (DataFilter.get_max(data[1]) > max_value) {
-                        max_value = DataFilter.get_max(data[1]);
-                    }
+			while (samples < num_samples)
+			{
+				data = board_shim.get_current_board_data();
+				if (data.GetLength(1) > 0)
+				{
+					DataFilter.perform_rolling_filter(data, 2, AggOperations.MEAN);
+					vals_mean += DataFilter.get_avg(data) / num_samples;
+					samples += data.GetLength(1);
+					if (DataFilter.get_max(data) > max_value)
+					{
+						max_value = DataFilter.get_max(data);
+					}
 				}
 			}
 
-		// add the code for calibration then set this blink_threshold
-		blink_thresh = 0.5 * (Math.pow((max_val - val_mean), 2));
+			// add the code for calibration then set this blink_threshold
+			blink_thresh = 0.5 * (Math.Pow((max_value - vals_mean), 2));
 
-		Debug.Log("mean value");
-		Debug.Log(vals_mean);
-		Debug.Log("max value");
-		Debug.Log(max_value);
-		Debug.Log("threshold");
-		Debug.Log(blink_thresh);
+			Debug.Log("mean value");
+			Debug.Log(vals_mean);
+			Debug.Log("max value");
+			Debug.Log(max_value);
+			Debug.Log("threshold");
+			Debug.Log(blink_thresh);
 
-		Debug.Log("CALIBRATION COMPLETE, START PLAYING");
-		
-		
+			Debug.Log("CALIBRATION COMPLETE, START PLAYING");
+
 		}
-	} catch (BrainFlowError as err) {
-		Debug.Log(err);
+		catch (BrainFlowError err)
+		{
+			Debug.Log(err);
+		}
 	}
-	
+
 	// Update is called once per frame
-    void Update () {
+	void Update()
+	{
 		if (board_shim == null)
 		{
 			return;
 		}
 
-		int data_points = sample_rate * 4;
-		
+		int data_points = (int)sampling_rate * 4;
 	}
 
-	private void onDestroy()
+	private void OnDestroy()
 	{
 		if (board_shim != null)
 		{
 			try
 			{
 				board_shim.release_session();
+			}
+			catch (BrainFlowError err)
+			{
+				Debug.Log(err);
 			}
 		}
 	}
